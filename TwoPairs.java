0f79fa01542ad1eafdf283611376049b9886e274
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-09 23:27:18 +0530

    "rewrite "

--- TwoPairs.java
+++ TwoPairs.java
@@ -1,25 +1,27 @@
-import java.util.Arrays;
-import java.util.*;
 public class TwoPairs {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5} ;
-        int[] arr1 = {} ;
+        int[] arr1 = new int[2] ;
         int target = 7 ; 
-         int i = 0 , j = arr.length + 1  ; 
-         while (i < j ) {
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if ((arr[i] + arr[j]) == target) {
+                    arr1[0] = i;
+                    arr1[1] = j;
+                    break ;
+                }
 
-                if((arr[i]  + arr[j] ) ==  target){
-                    arr[i] = arr1[0] ;
-                    arr[j] = arr1[1] ;
-                }
                 else{
-                    System.out.println("Element not matched");
+                    System.out.println("Pair not found");
                 }
-         }
-         
-         for (int j2 = 0; j2 < arr1.length; j2++) {
-            System.out.println(arr1[j2]);
-         }
+            }
+            for (int j = 0; j < arr1.length; j++) {
+                System.out.println();
+            }
+            
+        }
+        
     }
 
 }
