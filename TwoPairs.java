216f68b4842550b2e691e17c9b9ea193eda63d56
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-09 23:30:39 +0530

    "day 1 "

--- TwoPairs.java
+++ TwoPairs.java
@@ -1,27 +1,18 @@
 public class TwoPairs {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5} ;
-        int[] arr1 = new int[2] ;
-        int target = 7 ; 
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 7;
         boolean found = false;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length && !found; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if ((arr[i] + arr[j]) == target) {
-                    arr1[0] = i;
-                    arr1[1] = j;
-                    break ;
-                }
-
-                else{
-                    System.out.println("Pair not found");
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(i + " " + j);
+                  
+                    found = true;
+                   
                 }
             }
-            for (int j = 0; j < arr1.length; j++) {
-                System.out.println(arr1[j]);
-            }
-            
         }
-        
+        if (!found) System.out.println("Pair not found");
     }
-
 }
